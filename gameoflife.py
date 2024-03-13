652fbf1c25caf8d8d20658fe15d8c2975b27e04e
faun3 <antonioculcea@gmail.com>
2024-03-13 20:13:38 +0200

    feat: implement draw cell function

--- gameoflife.py
+++ gameoflife.py
@@ -1,7 +1,19 @@
 import pygame
 
+
+WINDOW_HEIGHT, WINDOW_WIDTH = 1000, 1000
+TILE_SIZE = 20
+BACKGROUND_COLOR = (36, 40, 36)
+FOREGROUND_COLOR = (232, 213, 176)
+
+
+def draw_cell(screen, row, col):
+    pygame.draw.rect(screen, FOREGROUND_COLOR, pygame.Rect(row * TILE_SIZE, col * TILE_SIZE, TILE_SIZE, TILE_SIZE))
+
+
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption("game of life")
 clock = pygame.time.Clock()
 running = True
 
@@ -10,7 +22,9 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill('gray')
+    screen.fill(BACKGROUND_COLOR)
+
+    draw_cell(screen, 1, 4)
 
     pygame.display.flip()
 
