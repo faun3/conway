508bdc10a2986db13dbf20ca05158ad2e0bdf38d
faun3 <antonioculcea@gmail.com>
2024-03-13 20:40:55 +0200

    feat: add random board generation

--- gameoflife.py
+++ gameoflife.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 
 WINDOW_HEIGHT, WINDOW_WIDTH = 1000, 1000
@@ -7,8 +8,32 @@ BACKGROUND_COLOR = (36, 40, 36)
 FOREGROUND_COLOR = (232, 213, 176)
 
 
-def draw_cell(screen, row, col):
-    pygame.draw.rect(screen, FOREGROUND_COLOR, pygame.Rect(row * TILE_SIZE, col * TILE_SIZE, TILE_SIZE, TILE_SIZE))
+def generate_random_board(live_density: float) -> list[list[int]]:
+    size = WINDOW_WIDTH // TILE_SIZE
+    generated_board = [[0 for _ in range(size)] for _ in range(size)]
+    for i in range(size):
+        for j in range(size):
+            if random.random() < live_density:
+                generated_board[i][j] = 1
+            else:
+                generated_board[i][j] = 0
+
+    return generated_board
+
+
+board = generate_random_board(0.15)
+
+
+def toggle_cell(game_board, row, col):
+    game_board[row][col] = 1 if game_board[row][col] == 0 else 0
+
+
+def draw_cells(game_screen, game_board):
+    size = WINDOW_WIDTH // TILE_SIZE
+    for i in range(size):
+        for j in range(size):
+            if game_board[i][j] == 1:
+                pygame.draw.rect(game_screen, FOREGROUND_COLOR, pygame.Rect(i * TILE_SIZE, j * TILE_SIZE, TILE_SIZE, TILE_SIZE))
 
 
 pygame.init()
@@ -24,7 +49,7 @@ while running:
 
     screen.fill(BACKGROUND_COLOR)
 
-    draw_cell(screen, 1, 4)
+    draw_cells(screen, board)
 
     pygame.display.flip()
 
